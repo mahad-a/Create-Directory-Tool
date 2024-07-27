ad2e2262ae9ec6456fe004c541f2fc50e1191320
Mahad Abdullahi Ahmed <59744373+mahad-a@users.noreply.github.com>
2024-07-27 18:03:49 -0400

    slight change to better handle indentations

--- src/create_project_structure.py
+++ src/create_project_structure.py
@@ -4,28 +4,24 @@ def create_structure(structure, root='.'):
     # grab the passed string structure and prepare to build project
     lines = structure.strip().split('\n')
     stack = []
-    current_indent = -1
+    current_indent = 0
 
     for line in lines: # for each line in the string (each file or directory)
         indent = len(line) - len(line.lstrip())
         name = line.strip()
         
-        # determine if it's a directory or a file
-        is_directory = not any(ext in name for ext in ('.',))
-        
         # adjust stack according to the current indentation
-        while indent <= current_indent:
+        while stack and indent <= stack[-1][1]:
             stack.pop()
-            current_indent -= 4
         
         # determine the current path 
-        current_path = os.path.join(root, *stack)
+        current_path = os.path.join(root, *[d[0] for d in stack])
         target_path = os.path.join(current_path, name)
 
-        if is_directory:
+        # determine if it's a directory or a file
+        if '.' not in name:
             os.makedirs(target_path, exist_ok=True)
-            stack.append(name)
-            current_indent = indent
+            stack.append((name, indent))
         else:
             os.makedirs(current_path, exist_ok=True)
             with open(target_path, 'w') as f:
@@ -37,30 +33,25 @@ def create_structure_from_file(file_path, root='.'):
         lines = file.readlines()
     
     stack = []
-    current_indent = -1
+    current_indent = 0
 
     for line in lines: # for each line in the textfile
         indent = len(line) - len(line.lstrip())
         name = line.strip()
         
-        # determine if it's a directory or a file
-        is_directory = not any(ext in name for ext in ('.',))
-        
         # adjust stack according to the current indentation
-        while indent <= current_indent:
+        while stack and indent <= stack[-1][1]:
             stack.pop()
-            current_indent -= 4
         
         # determine the current path
-        current_path = os.path.join(root, *stack)
+        current_path = os.path.join(root, *[d[0] for d in stack])
         target_path = os.path.join(current_path, name)
 
-        if is_directory:
+        # determine if it's a directory or a file
+        if '.' not in name:
             os.makedirs(target_path, exist_ok=True)
-            stack.append(name)
-            current_indent = indent
+            stack.append((name, indent))
         else:
             os.makedirs(current_path, exist_ok=True)
             with open(target_path, 'w') as f:
                 pass  # create an empty file
-
